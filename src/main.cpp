3ce2ab2a410ac68cde284bb5a9673228866406ba
Lars Olav Tolo <h141465@stud.hib.no>
2016-05-12 13:48:44 +0200

    Drawing quads instead of teapot

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,54 @@
 #include <osg/Texture2D>
 #include <osg/Geometry>
 #include <osg/Geode>
+#include <osg/Group>
+#include <osg/PositionAttitudeTransform>
 
 const int TEXTURE_WIDTH = 512;
 const int TEXTURE_HEIGHT = 512;
+
+void addQuad(osg::Group* scene, osg::Vec3 position, osg::Vec4 color)
+{
+	if (!scene) return;
+
+	static osg::ref_ptr<osg::Vec3Array> vertexArray;
+	if (!vertexArray)
+	{
+		vertexArray = new osg::Vec3Array();
+		vertexArray->push_back(osg::Vec3(-1.f, -1.f, 0.f));
+		vertexArray->push_back(osg::Vec3(1.f, -1.f, 0.f));
+		vertexArray->push_back(osg::Vec3(1.f, 1.f, 0.f));
+		vertexArray->push_back(osg::Vec3(-1.f, 1.f, 0.f));
+	}
+
+	osg::ref_ptr<osg::Vec4Array> colorArray;
+	osg::ref_ptr<osg::Geometry> quadGeometry;
+	quadGeometry = new osg::Geometry();
+	quadGeometry->setVertexArray(vertexArray);
+	colorArray = new osg::Vec4Array();
+	colorArray->push_back(color);
+	quadGeometry->setColorArray(colorArray);
+	quadGeometry->setColorBinding(osg::Geometry::BIND_OVERALL);
+	quadGeometry->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLE_FAN, 0, 4));
+	
+	osg::Geode* geode = new osg::Geode();
+	geode->addDrawable(quadGeometry);
+
+	osg::PositionAttitudeTransform* transform = new osg::PositionAttitudeTransform();
+	transform->addChild(geode);
+	transform->setPosition(position);
+
+	scene->addChild(transform);
+}
+
+osg::ref_ptr<osg::Group> createScene()
+{
+	osg::ref_ptr<osg::Group> group = new osg::Group();
+	addQuad(group.get(), osg::Vec3(-0.25f, 0.f, 0.f), osg::Vec4(1.f, 0.f, 0.f, 0.5f));
+	addQuad(group.get(), osg::Vec3(0.25f, 0.f, 0.25f), osg::Vec4(0.f, 1.f, 0.f, 0.3f));
+	addQuad(group.get(), osg::Vec3(0.f, 0.25f, -0.25f), osg::Vec4(0.f, 0.f, 1.f, 0.36f));
+	return group;
+}
 
 int main(int argc, char** argv)
 {
@@ -13,7 +58,7 @@ int main(int argc, char** argv)
 	texCamera->setReferenceFrame(osg::Camera::ABSOLUTE_RF);
 	texCamera->setRenderOrder(osg::Camera::PRE_RENDER);
 	texCamera->setProjectionMatrixAsPerspective(60.0, 1.0, 0.1, 100.0);
-	texCamera->setViewMatrixAsLookAt(osg::Vec3(0.f, 1.f, 10.f), osg::Vec3(0.f, 0.f, 0.f), osg::Vec3(0.f, 1.f, 0.f));
+	texCamera->setViewMatrixAsLookAt(osg::Vec3(0.f, 1.f, 4.f), osg::Vec3(0.f, 0.f, 0.f), osg::Vec3(0.f, 1.f, 0.f));
 	texCamera->setViewport(0, 0, TEXTURE_WIDTH, TEXTURE_HEIGHT);
 	texCamera->setClearColor(osg::Vec4(0.5f, 0.5f, 0.5f, 1.f));
 	texCamera->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -24,8 +69,8 @@ int main(int argc, char** argv)
 	osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D(image);
 	texCamera->attach(osg::Camera::COLOR_BUFFER, texture.get());
 
-	osg::ref_ptr<osg::Node> teapot = osgDB::readRefNodeFile("teapot.osg");
-	texCamera->addChild(teapot.get());
+	osg::ref_ptr<osg::Group> texScene = createScene();
+	texCamera->addChild(texScene.get());
 
 	osg::ref_ptr<osg::Geode> quad = new osg::Geode();
 	osg::ref_ptr<osg::Geometry> quadGeometry = new osg::Geometry();
